99384728c8cebb6a57591a5f9f4e28da57c1b854
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-10-18 16:20:49 +0300

    Add #downloaded

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,14 @@ import * as apiRequests from './requests.js';
 
 import MediaList from './views/mediaList.js';
 import MediaDetails from './views/mediaDetails.js';
+import DownloadedList from './views/downloadedList.js';
 
 const view = document.getElementById('view');
 
 const routes = {
   list: showMediaList,
   details: showMediaDetails,
+  downloaded: showDownloaded,
 };
 
 const searchInput = document.getElementById('searchShowsInput');
@@ -38,6 +40,12 @@ async function showMediaDetails(id) {
   page.render();
 };
 
+async function showDownloaded() {
+  const mediaItems = await apiRequests.fetchCurrentPrograms();
+  const page = new DownloadedList(view, mediaItems);
+  page.render();
+};
+
 // Add eventlistener to watch changes in search form
 searchInput.addEventListener('change', getMatchingShows);
 searchInput.addEventListener('keyup', getMatchingShows);
@@ -58,6 +66,9 @@ async function handleRouteChange() {
       break;
     case 'details':
       routes.details(segments[1]);
+      break;
+    case 'downloaded':
+      routes.downloaded();
       break;
     default:
       break;
---
+++ app/scripts/views/downloadedList.js
@@ -0,0 +1,30 @@
+/**
+ * View for viewing downloaded videos.
+ */
+export default class DownloadedList {
+  /**
+   * Default constructor for setting the values
+   *
+   * @param {HTMLElement} element - The HTML element to bind/adopt
+   * @param {Array<Object>} [mediaItems=null] - The array containing media items
+   */
+  constructor(element, mediaItems = null) {
+    this.element = element;
+    this.mediaItems = mediaItems;
+  }
+
+  /**
+   * Render page.
+   */
+  render() {
+    this.element.innerHTML = '';
+    this.mediaItems.forEach((media) => {
+        this.element.innerHTML += `
+          <a href="#downloaded/${media.getId()}"><div class="mdc-card">
+            <i class="material-icons">play arrow</i>
+            <h5 class="image-title">${media.getTitle()}</h1>
+          </div></a>
+        `;
+    });
+  }
+}
