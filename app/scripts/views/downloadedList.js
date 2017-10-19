e5d27661c3b801b2c5893a06367a04cd1ca6de8d
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-10-19 11:00:50 +0300

    Add delete icon to downloaded list

--- app/scripts/views/downloadedList.js
+++ app/scripts/views/downloadedList.js
@@ -33,6 +33,9 @@ export default class DownloadedList {
           </span>
           <span class="lRight">
             <a href="#download/${episode.getId()}/${episode.getMediaId()}">
+              <i class="material-icons media-control-buttons arial-label="Stream">
+                delete_forever
+              </i>
               <i class="material-icons media-control-buttons" arial-label="Stream">
                 play_circle_filled
               </i>
