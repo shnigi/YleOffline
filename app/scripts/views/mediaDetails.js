429e4cb69150c23df6cd10925a4e2409905ae43d
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-10-19 14:42:24 +0300

    A little bit of formatting

--- app/scripts/views/mediaDetails.js
+++ app/scripts/views/mediaDetails.js
@@ -3,22 +3,22 @@ import * as videoMagic from '../videoMagic.js';
  * View for showing media item's details
  */
 export default class MediaDetails {
-    constructor(element, media = null) {
-      this.element = element;
-      this.media = media;
-      // this.supportsPUSH = false;
+  constructor(element, media = null) {
+    this.element = element;
+    this.media = media;
+    // this.supportsPUSH = false;
+  }
+  render() {
+    let progImage = 'images/no-image.jpg';
+    let imageId = '';
+    if (this.media.program.image || this.media.program.coverImage) {
+      imageId = this.media.program.image.id || this.media.program.coverImage.id;
+      progImage = `http://images.cdn.yle.fi/image/upload/${imageId}.jpg`;
     }
-    render() {
-        let progImage = 'images/no-image.jpg';
-        let imageId = '';
-        if (this.media.program.image || this.media.program.coverImage) {
-          imageId = this.media.program.image.id || this.media.program.coverImage.id;
-          progImage = `http://images.cdn.yle.fi/image/upload/${imageId}.jpg`;
-        }
-        const progTitle = this.media.program.title.fi || this.media.program.title.sv;
-        const progDescription = this.media.program.description.fi ||
-                                this.media.program.description.sv || '';
-        let html = `
+    const progTitle = this.media.program.title.fi || this.media.program.title.sv;
+    const progDescription = this.media.program.description.fi ||
+      this.media.program.description.sv || '';
+    let html = `
         <div class="mdc-card card-image">
           <i class="material-icons back-arrow" id="goBack">arrow_back</i>
           <h2 class="image-title">${progTitle}</h2>
@@ -30,17 +30,17 @@ export default class MediaDetails {
         <section>
           <ul class="listaus">
           `;
-      if (this.media.episodes.length > 0) {
-          this.media.episodes.forEach((episode) => {
-            html += `
+    if (this.media.episodes.length > 0) {
+      this.media.episodes.forEach((episode) => {
+        html += `
             <li>
               <span class="lLeft">
                 <span class="lTitle">${episode.getTitle()}</span>`;
 
-            if (episode.getDuration() !== ' ') {
-                html += `<span class="lDur">Kesto ${episode.getDuration()} min</span>`;
-            }
-           html += `</span>
+        if (episode.getDuration() !== ' ') {
+          html += `<span class="lDur">Kesto ${episode.getDuration()} min</span>`;
+        }
+        html += `</span>
               <span class="lRight">
                 <a id="${episode.getId()}-${episode.getMediaId()}"><i class="material-icons media-control-buttons" arial-label="Download">file_download</i></a>
                 <a href="#playStream/${episode.getId()}/${episode.getMediaId()}""><i class="material-icons media-control-buttons" arial-label="Stream">play_circle_filled</i></a>
@@ -48,38 +48,36 @@ export default class MediaDetails {
             </li>
             <hr>
             `;
-            });
-          html += `
+      });
+      html += `
             </ul>
           </section>
         `;
-        this.element.innerHTML = html;
-        this.media.episodes.forEach((episode) => {
-          document.getElementById(`${episode.getId()}-${episode.getMediaId()}`).addEventListener('click',
-            () => videoMagic.initDownload(episode));
-        });
-      }
-      else {
-        // Jos ohjelmalla ei ole jaksoja, luodaan tietokortti sille itselleen.
-        html += `
+      this.element.innerHTML = html;
+      this.media.episodes.forEach((episode) => {
+        document.getElementById(`${episode.getId()}-${episode.getMediaId()}`).addEventListener('click',
+          () => videoMagic.initDownload(episode));
+      });
+    }
+    else {
+      // Jos ohjelmalla ei ole jaksoja, luodaan tietokortti sille itselleen.
+      console.log(this.media);
+      html += `
               <li>
                 <span class="lLeft">
                   <span class="lTitle">${progTitle}</span>`;
-        html += `</span>
+      html += `</span>
                 <span class="lRight">
                   <a href="#download/${imageId}/${imageId}"><i class="material-icons media-control-buttons" arial-label="Download">file_download</i></a>
                   <a href="#playStream/${imageId}/${imageId}""><i class="material-icons media-control-buttons" arial-label="Stream">play_circle_filled</i></a>
                 </span>
               </li>`;
-      }
-      html += `
+    }
+    html += `
           </ul>
           </section>
           `;
-      this.element.innerHTML = html;
-      console.log("median yksityiskohtia tässä vaan rendataan, höhöö");
-      // Attach the scroller - this is done after templating to permit the right behaviour
-      // const tabBarScroller =
-      //  new MDCTabBarScroller(document.querySelector('.mdc-tab-bar-scroller'));
-    }
+    this.element.innerHTML = html;
+
+  }
 }
