53682170adca0bc622daa5f1e1ab9becb79e5ca6
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-10-19 14:22:55 +0300

    Show play controls in single shows

--- app/scripts/views/mediaDetails.js
+++ app/scripts/views/mediaDetails.js
@@ -2,23 +2,24 @@
  * View for showing media item's details
  */
 export default class MediaDetails {
-    constructor(element, media = null) {
-      this.element = element;
-      this.media = media;
-      // this.supportsPUSH = false;
-    }
+  constructor(element, media = null) {
+    this.element = element;
+    this.media = media;
+    // this.supportsPUSH = false;
+  }
 
-    render() {
-        let progImage = 'images/no-image.jpg';
-        if (this.media.program.image || this.media.program.coverImage) {
-          const imageId = this.media.program.image.id ||
-                        this.media.program.coverImage.id;
-          progImage = `http://images.cdn.yle.fi/image/upload/${imageId}.jpg`;
-        }
-        const progTitle = this.media.program.title.fi || this.media.program.title.sv;
-        const progDescription = this.media.program.description.fi ||
-                                this.media.program.description.sv || '';
-        let html = `
+  render() {
+    let progImage = 'images/no-image.jpg';
+    let imageId = '';
+    if (this.media.program.image || this.media.program.coverImage) {
+      imageId = this.media.program.image.id || this.media.program.coverImage.id;
+      progImage = `http://images.cdn.yle.fi/image/upload/${imageId}.jpg`;
+    }
+    const progTitle = this.media.program.title.fi || this.media.program.title.sv;
+    const progDescription = this.media.program.description.fi ||
+      this.media.program.description.sv || '';
+    console.log(this.media);
+    let html = `
         <div class="mdc-card card-image">
           <i class="material-icons back-arrow" id="goBack">arrow_back</i>
           <h2 class="image-title">${progTitle}</h2>
@@ -30,16 +31,17 @@ export default class MediaDetails {
         <section>
           <ul class="listaus">
           `;
-          this.media.episodes.forEach((episode) => {
-            html += `
+    if (this.media.episodes.length > 1) {
+      this.media.episodes.forEach((episode) => {
+        html += `
             <li>
               <span class="lLeft">
                 <span class="lTitle">${episode.getTitle()}</span>`;
 
-            if (episode.getDuration() !== ' ') {
-                html += `<span class="lDur">Kesto ${episode.getDuration()} min</span>`;
-            }
-           html += `</span>
+        if (episode.getDuration() !== ' ') {
+          html += `<span class="lDur">Kesto ${episode.getDuration()} min</span>`;
+        }
+        html += `</span>
               <span class="lRight">
                 <a href="#download/${episode.getId()}/${episode.getMediaId()}"><i class="material-icons media-control-buttons" arial-label="Download">file_download</i></a>
                 <a href="#playStream/${episode.getId()}/${episode.getMediaId()}""><i class="material-icons media-control-buttons" arial-label="Stream">play_circle_filled</i></a>
@@ -47,14 +49,28 @@ export default class MediaDetails {
             </li>
             <hr>
             `;
-            });
-          html += `
-            </ul>
-          </section>
-        `;
-        this.element.innerHTML = html;
-      // Attach the scroller - this is done after templating to permit the right behaviour
-      // const tabBarScroller =
-      //  new MDCTabBarScroller(document.querySelector('.mdc-tab-bar-scroller'));
+      });
+    } else {
+      // Jos ohjelmalla ei ole jaksoja, luodaan tietokortti sille itselleen.
+      html += `
+            <li>
+              <span class="lLeft">
+                <span class="lTitle">${progTitle}</span>`;
+      html += `</span>
+              <span class="lRight">
+                <a href="#download/${imageId}/${imageId}"><i class="material-icons media-control-buttons" arial-label="Download">file_download</i></a>
+                <a href="#playStream/${imageId}/${imageId}""><i class="material-icons media-control-buttons" arial-label="Stream">play_circle_filled</i></a>
+              </span>
+            </li>`;
     }
+    html += `
+        </ul>
+        </section>
+        `;
+    this.element.innerHTML = html;
+    console.log("median yksityiskohtia tässä vaan rendataan, höhöö");
+    // Attach the scroller - this is done after templating to permit the right behaviour
+    // const tabBarScroller =
+    //  new MDCTabBarScroller(document.querySelector('.mdc-tab-bar-scroller'));
+  }
 }
